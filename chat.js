3b98c4a30ebaef5f1ae78295079e06c5a7ae8485
Alexak-99 <alex70528283@gmail.com>
2026-06-30 17:46:45 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,4 +1,4 @@
- const messages = document.getElementById("messages");
+const messages = document.getElementById("messages");
 const input = document.getElementById("messageInput");
 
 function getTime() {
@@ -14,19 +14,39 @@ function sendMessage() {
 
     const msg = document.createElement("div");
     msg.className = "sent";
+
     msg.innerHTML = `
-        ${text}
-        <div class="time">${getTime()}</div>
+        <div>${text}</div>
+
+        <div class="message-info">
+            <span class="time">${getTime()}</span>
+
+            <span class="status">
+                <span class="dot"></span>
+                <span class="dot"></span>
+            </span>
+        </div>
     `;
 
     messages.appendChild(msg);
 
     input.value = "";
-
     messages.scrollTop = messages.scrollHeight;
 
-    setTimeout(() => {
+    // Pehle Red Dot
+    const dots = msg.querySelectorAll(".dot");
 
+    dots.forEach(dot => {
+        dot.style.background = "red";
+    });
+
+    // 1 second baad Green Dot
+    setTimeout(() => {
+        dots.forEach(dot => {
+            dot.style.background = "#00ff66";
+        });
+
+        // Demo Auto Reply
         const reply = document.createElement("div");
         reply.className = "received";
         reply.innerHTML = `
@@ -35,17 +55,13 @@ function sendMessage() {
         `;
 
         messages.appendChild(reply);
-
         messages.scrollTop = messages.scrollHeight;
 
-    },1000);
-
+    }, 1000);
 }
 
-input.addEventListener("keydown",function(e){
-
-if(e.key==="Enter"){
-sendMessage();
-}
-
+input.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        sendMessage();
+    }
 });
