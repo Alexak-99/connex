e1263e8ce68a1c605ded447152a356402c42100c
Alexak-99 <alex70528283@gmail.com>
2026-07-14 20:54:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,32 @@
 setTimeout(function () {
     window.location.href = "login.html";
 }, 2500);
+const searchInput = document.getElementById("searchInput");
+
+if(searchInput){
+
+searchInput.addEventListener("keyup", function(){
+
+const value = this.value.toLowerCase();
+
+const chats = document.querySelectorAll(".chat");
+
+chats.forEach(chat=>{
+
+const name = chat.querySelector("h3").innerText.toLowerCase();
+
+if(name.includes(value)){
+
+chat.style.display="flex";
+
+}else{
+
+chat.style.display="none";
+
+}
+
+});
+
+});
+
+}
