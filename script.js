620114af0276101ffd23b8174cddb30c0ae24f84
Alexak-99 <alex70528283@gmail.com>
2026-07-17 12:30:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
- // Splash Screen Redirect (sirf index.html ke liye)
+ // Splash Screen Redirect (sirf index page ke liye)
 
-if (window.location.pathname.includes("index.html")) {
-
+if (
+    window.location.pathname.endsWith("/") ||
+    window.location.pathname.endsWith("/index.html")
+) {
     setTimeout(function () {
         window.location.href = "login.html";
     }, 2500);
-
 }
 
 const searchInput = document.getElementById("searchInput");
 
-if(searchInput){
+if (searchInput) {
 
-searchInput.addEventListener("keyup", function(){
+    searchInput.addEventListener("keyup", function () {
 
-const value = this.value.toLowerCase();
+        const value = this.value.toLowerCase();
 
-const chats = document.querySelectorAll(".chat");
+        const chats = document.querySelectorAll(".chat");
 
-chats.forEach(chat=>{
+        chats.forEach(chat => {
 
-const name = chat.querySelector("h3").innerText.toLowerCase();
+            const name = chat.querySelector("h3").innerText.toLowerCase();
 
-if(name.includes(value)){
+            if (name.includes(value)) {
+                chat.style.display = "flex";
+            } else {
+                chat.style.display = "none";
+            }
 
-chat.style.display="flex";
+        });
 
-}else{
-
-chat.style.display="none";
-
-}
-
-});
-
-});
+    });
 
 }
